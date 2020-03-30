47a0b7bc5431d81e443db6533838ac1cdf5cb833
hmmatthews1205 <47952223+hmmatthews1205@users.noreply.github.com>
2020-03-30 10:43:34 -0500

    Add project files.

---
+++ WebAppSGL1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppSGL1.Startup))]
+namespace WebAppSGL1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
