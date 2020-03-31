080d8c39ea599694632c48741a05e4ebd6e92838
hmmatthews1205 <47952223+hmmatthews1205@users.noreply.github.com>
2020-03-31 11:10:01 -0500

    refactoring code

--- WebAppSGL1/Scripts/MySignalR.js
+++ WebAppSGL1/Scripts/MySignalR.js
@@ -1,15 +1,23 @@
-﻿$.connection.hub.start()
-    .done(function ()
-    {
-        //console.log("My SignalR Connection was successfull!")
-        $.connection.myHhub01.server.announce("My SignalR Connection was successfull!");
-    })
-    .fail(function () {
-        alert("Connection error...")
-    });
+﻿(function () {
+    var MyHub = $.connection.myHhub01;
 
-$.connection.myHhub01.client.announce = function (message) {
+    $.connection.hub.start()
+        .done(function () {
+            //console.log("My SignalR Connection was successfull!")
+            writeToPage("It worked ! ");
+            MyHub.server.announce("XXX My SignalR Connection was successfull!");
+        })
+        .fail(function () {
+            writeToPage("Connection error...");
+        });
 
-    $("#welcome-messages").append(message + "<br />");
-  
-}
+    MyHub.client.announce = function (message) {
+
+        writeToPage(message); 
+
+    }
+
+    var writeToPage = function(message){
+        $("#welcome-messages").append(message + "<br />");
+    }
+})()
